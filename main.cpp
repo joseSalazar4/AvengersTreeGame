a7c63e6ea98d684ccf958a29ceb207e504497cc9
Jose <mikomeka1@gmail.com>
2019-10-20 14:17:59 -0600

    Feature built: send emails with Files created in execution

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
-
+#include "iostream"
+#include "fstream"
 #include <QApplication>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    ofstream myfile;
+    myfile.open ("example.txt");
+    myfile << "Writing this to a file.\n";
+    myfile << "NO me dejare vencer no los dejare llevarme.\n NUnca nadie meva a dominar no yo voy a pelear";
+    myfile.close();
+
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "smtp.h"
+#include "qlist.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +15,13 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_pushButton_clicked()
+{
+
+    Smtp* smtp = new Smtp("mikomeka1@gmail.com", "mikomeka11", "smtp.gmail.com", 465);
+    connect(smtp, SIGNAL(status(QString)), this, SLOT(mailSent(QString)));
+    QList<QString> archivos = {"example.txt"};
+
+    smtp->sendMail("mikomeka1@gmail.com", "jsalazarg48@gmail.com" , "No no o quiero ver","ESto es \n solo una \n prueba para ver si los enters funcionan", archivos);
+
+}
